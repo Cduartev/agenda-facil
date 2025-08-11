ccdde9796460de9a633021cdf8c0e1042100a9e1
Cduartev <cayooduartevidal@gmail.com>
2025-08-11 17:49:21 -0300

    refactor: testando pois ele nao esta reconhecendo meu use client

--- src/app/confirmar-email/page.tsx
+++ src/app/confirmar-email/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import React, { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
 import { CheckCircle, Home, Mail, Clock, XCircle } from "lucide-react";
